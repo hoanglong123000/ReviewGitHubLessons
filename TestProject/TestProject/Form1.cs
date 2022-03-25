80f49833a27d7a7c14b269c8b55311ee13742f2c
hoanglong123000 <hoanglong123000@gmail.com>
2022-03-25 09:09:44 +0700

    Developer1: Add a comment at line 14.

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -11,10 +11,15 @@ using System.Windows.Forms;
 namespace TestProject
 {
     public partial class Form1 : Form
-    {
+    {   // comment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
